9a9f648382836b9405eda5b3d205779aa90c3019
= <phan.ngoclan58@gmail.com>
2017-06-22 14:22:09 +0700

    Changed deployment chain

--- ft_deploy.sh
+++ ft_deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 rm fastText/*
-scrapy crawl dantri
-scrapy crawl danviet
-scrapy crawl vnexpress
-scrapy crawl kenh14
-scrapy crawl pl
-scrapy crawl thanhnien
-scrapy crawl tienphong
-scrapy crawl vietbao
-scrapy crawl vietnamnet
-scrapy crawl vnplus
-scrapy crawl vov
-scrapy crawl vtv
-scrapy crawl btt
-scrapy crawl ttvh
-scrapy crawl gdn
-scrapy crawl ktn
+scrapy crawl dantri &&\
+scrapy crawl danviet &&\
+scrapy crawl vnexpress &&\
+scrapy crawl kenh14 &&\
+scrapy crawl pl &&\
+scrapy crawl thanhnien &&\
+scrapy crawl tienphong &&\
+scrapy crawl vietbao &&\
+scrapy crawl vietnamnet &&\
+scrapy crawl vnplus &&\
+scrapy crawl vov &&\
+scrapy crawl vtv &&\
+scrapy crawl btt &&\
+scrapy crawl ttvh &&\
+scrapy crawl gdn &&\
+scrapy crawl ktn &&\
 scrapy crawl ndh
