2901c6dbc73ea8856c5a3d7d3980b3db04f93f83
= <phan.ngoclan58@gmail.com>
2018-08-13 10:16:20 +0700

    Misc changes

--- crawl_all.sh
+++ crawl_all.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
-scrapy crawl dantri &&\
-scrapy crawl danviet &&\
-scrapy crawl vnexpress &&\
-scrapy crawl kenh14 &&\
-scrapy crawl pl &&\
-scrapy crawl thanhnien &&\
-scrapy crawl tienphong &&\
-scrapy crawl vietbao &&\
-scrapy crawl vietnamnet &&\
-scrapy crawl vnplus &&\
-scrapy crawl vov &&\
-scrapy crawl vtv &&\
-scrapy crawl btt &&\
-scrapy crawl ttvh &&\
-scrapy crawl gdn &&\
-scrapy crawl ktn &&\
-scrapy crawl ndh
+set -e
+
+scrapy list | \
+    while read SPIDER; do
+        CMD="scrapy crawl $SPIDER"
+        echo ${CMD}
+        eval ${CMD}
+    done
